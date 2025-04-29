1cbfc9aa318aa7bae4a9de28285dd22b8e370f2f
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2025-04-29 17:41:25 +0500

    update

--- app/components/googlelogin/GoogleLogin.tsx
+++ app/components/googlelogin/GoogleLogin.tsx
@@ -20,7 +20,7 @@ export default function GoogleLogin() {
     const clientId =
       "511465103962-nkrpa9jm2qcjmk2brcmd3scc3shfg4qt.apps.googleusercontent.com";
     const redirectUri = encodeURIComponent(
-      "http://localhost:8000/api/auth/google/callback"
+      "https://medizen-backend-three.vercel.app/api/auth/google/callback"
     );
     const scope = encodeURIComponent("email profile");
 
